4b62bd9776a2f1f062e65be04724e9629c4859d8
Sergio Filho <serginhoadeus@gmail.com>
2019-09-06 10:12:30 -0300

    teste

--- schema-query/projeto/index.js
+++ schema-query/projeto/index.js
@@ -1,7 +1,42 @@
 const { ApolloServer, gql} = require('apollo-server')
 
+
+const usuarios = [
+    {
+    id: 1,
+    nome: 'Sergio',
+    email: 'sergio@email',
+    idade: 24,
+    perfil_id : 1,
+   }, 
+    {
+    id: 2,
+    nome: 'Sergio filho',
+    email: 'sergio@email',
+    idade: 25,
+    perfil_id : 2,
+   },
+   {
+    id: 3,
+    nome: 'Sthiago tx',
+    email: 'sergio@email',
+    idade: 25,
+    perfil_id : 1,
+    }
+]
+const perfil = [
+{id: 1,nome: ' Perfil 1 Comum'},
+{id: 2,nome: 'Perfil 2 Superuser'}
+]
 const typeDefs = gql`
     scalar Date
+
+    type Produto {
+        nome: String!
+        preco: Float!
+        desconto: Float
+        precoComDesconto: Float      
+    }
 
     type Usuario {
         id: ID
@@ -10,6 +45,12 @@ const typeDefs = gql`
         idade: Int
         salario: Float
         vip: Boolean
+        perfil: Perfil
+    }
+
+    type Perfil {
+        id: Int
+        nome: String!
     }
 
     # pontos de entradas da Api ! enterpoints
@@ -17,11 +58,34 @@ const typeDefs = gql`
         ola: String! 
         horaAtual: Date!
         usuarioLogado: Usuario
+        produtoEmDestaque: Produto
+        numerosMegaSena: [Int!]!
+        usuarios: [Usuario!]!
+        usuario(id: ID): Usuario!
+        perfil: [Perfil!]!
+        perfil_unico (id: Int) : Perfil!
     }
 
 `
 // resolvers , um resolver para cada consulta
 const resolvers = {
+    Usuario:{
+        salario(usuario){
+            return usuario.salario_real
+        }
+    },
+    
+    Produto : { 
+    precoComDesconto(produto) {
+      
+        if(produto.desconto){
+           return produto.preco * (1 - produto.desconto)
+       } else {
+           return produto.preco
+       }
+
+    }
+  },
     Query :{
         ola(){
             return 'String de retorno'
@@ -38,11 +102,44 @@ const resolvers = {
                 nome: 'Sergio',
                 email:'sergio@git.com.br',
                 idade: 24,
-                salario: 17.200,
+                salario_real: 17.200,
                 vip: false
             }
 
+        },
+        produtoEmDestaque(){
+            return{
+                nome: 'Teclado Gamer',
+                preco: 200.0,
+                desconto: 0.5,
+            }
+
+        },
+        numerosMegaSena(){
+            // return [5,9,12,27,33,55] valores amarrados ,  TODO: desafio remover os numeros repetidos
+            const crescente = (a, b) => a - b 
+            return Array(6).fill(0)
+                .map(n => parseInt(Math.random() * 60 + 1))
+                .sort(crescente)
+        },
+        usuarios(){
+            return usuarios
+        },
+        usuario(_, args){
+            const selecionados = usuarios.filter(u => u.id == args.id)
+
+            return selecionados ? selecionados[0] : null
+                                
+        },
+        perfil(){
+            return perfil
+        },
+        perfil_unico(_, args) {
+            const selecionados = perfil.filter(u => u.id == args.id)
+
+            return selecionados ? selecionados[0] : null    
         }
+        
     },
     
 }
